29abe297ae561443ed30bd65feedf9d35e37fa5c
ggw2004 <91301141+ggw2004@users.noreply.github.com>
2021-11-22 12:01:14 -0600

    created the grid for 2048

--- sketch.js
+++ sketch.js
@@ -1,15 +1,25 @@
-// Project Title
-// Your Name
+// 2048 Rip Off Version
+// George Williams
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let grid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = [
+    [0,0,0,0], 
+    [0,0,0,0], 
+    [0,0,0,0], 
+    [0,0,0,0]
+  ];
 }
 
 function draw() {
   background(220);
 }
+
+
+function addNumber() {}
