ec8957cc0144c5c965985521e50d80696d8c0ccf
ggw2004 <91301141+ggw2004@users.noreply.github.com>
2021-11-23 12:01:43 -0600

    Add the initial numbers of the game

--- sketch.js
+++ sketch.js
@@ -7,6 +7,7 @@
 
 let grid;
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = [
@@ -15,6 +16,10 @@ function setup() {
     [0,0,0,0], 
     [0,0,0,0]
   ];
+  console.table(grid);
+  addNumber();
+  addNumber();
+  console.table(grid);
 }
 
 function draw() {
@@ -22,4 +27,26 @@ function draw() {
 }
 
 
-function addNumber() {}
+function addNumber() {
+  let options = [];
+  let gridSize = 4;
+  for (let i = 0; i < gridSize; i ++) {
+    for (let j = 0; j < gridSize; j ++ ){
+      options.push({
+        x: i,
+        y: j,
+      });
+    }
+  }
+
+  if (options.length > 0) { 
+    let spot = random (options);
+    let r = random(1);
+    if (r > 0.5) {
+      grid[spot.x][spot.y] = 2;
+    }
+    else {
+      grid[spot.x][spot.y] = 4;
+    }
+  }
+}
