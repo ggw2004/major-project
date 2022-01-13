ef5f4a4cc268b774a334c5ce73930696ff5387b3
ggw2004 <91301141+ggw2004@users.noreply.github.com>
2022-01-13 11:06:13 -0600

    updated keystate to direction

--- sketch.js
+++ sketch.js
@@ -4,21 +4,20 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+ 
 let grid = [];
 let gridSize = 4;
 let rows = 4;
 let cols = 4;
 let cellWidth;
 let cellHeight;
-let letterSize = 64;
-
+ 
+ 
+let gameState;
 let direction;
-
-
-let keyState;
-
-
+ 
+ 
+ 
 function setup() {
   // create canvas based on the length of the shortest part of the screen
   if (windowWidth < windowHeight) {
@@ -27,34 +26,28 @@ function setup() {
   else {
     createCanvas (windowHeight * 0.8, windowHeight * 0.8);
   }
-
-  
+ 
+ 
   createGrid();
-  // grid = [
-  //   [0,0,0,0], 
-  //   [0,0,0,0], 
-  //   [0,0,0,0], 
-  //   [0,0,0,0]
-  // ];
-  console.table(grid);
+ 
+ 
+  // spawn first two numbers
   spawnNumber();
   spawnNumber();
-  console.table(grid);
-
 }
-
-
+ 
+ 
 // main game loop
 function draw() {
   background(255);
   cellWidth = width / 4;
   cellHeight = height / 4;
-
+ 
   create2DArray();
-
-
+ 
+ 
 }
-
+ 
 // create grid
 function createGrid() {
   for (let i = 0; i < gridSize; i++) {
@@ -64,49 +57,59 @@ function createGrid() {
     }
   }
 }
-
+ 
 // Movement Keys
 function keyPressed() {
   // Key to move up
   if (keyCode === 38 || keyCode === 87) { // 37 = UP_ARROW and 87 = w
-    keyState = "up";
+    direction = "up";
     moveVertical();
   }
-
+ 
   // Key to move down
   else if (keyCode === 40 || keyCode === 83) { // 40 = DOWN_ARROW and 83 = s
-    keyState = "down";
+    direction = "down";
     moveVertical();
   }
-
+ 
   // Key to move right
   if (keyCode === 39 || keyCode === 68) { // 39 = RIGHT_ARROW and 68 = d
-    keyState = "right";
+    direction = "right";
     moveHorizontal();
   }
-
+ 
   // Key to move left
   if (keyCode === 37 || keyCode === 65) { // 37 = LEFT_ARROW and 65 = w
-    keyState = "left";
+    direction = "left";
     moveHorizontal();
-
   }
-
-  // horizontal movement in for loop change j co-ordinates until there is a zero in the next cell
-
-  if (key === " ") {
-
-    let newCopy = gridCopy(grid);
-    for (let i = 0; i < gridSize; i++) {
-      grid[i] = horizontalOperate(grid[i]);
-    }
-    let changed = compareGrids(newCopy, grid);
-    if (changed) {
-      spawnNumber();
-    }
+ 
+  if (isGameOver()) {
+    console.log("game over");
   }
 }
-
+ 
+ 
+// check if there are any moves left (if the game is over)
+function isGameOver() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] === 0) {
+        return false;
+      }
+      if (i !== 3 && grid[i][j] === grid[i + 1][j]) {
+        return false;
+      }
+      if (j !== 3 && grid[i][j] === grid[i][j + 1]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+ 
+ 
+// compare grid copies
 function compareGrids(a , b) {
   for (let i = 0; i < gridSize; i ++) {
     for (let j = 0; j < gridSize; j ++ ){
@@ -117,61 +120,64 @@ function compareGrids(a , b) {
   }
   return false;
 }
-
+ 
+ 
+// create gid copy
 function gridCopy(grid) {
   let newCopy = [];
-
+ 
   for (let i = 0; i < gridSize; i++) {
     newCopy[i] = [];
     for (let j = 0; j < gridSize; j++) {
       newCopy[i][j] = 0;
     }
   }
-
+ 
   for (let i = 0; i < gridSize; i ++) {
     for (let j = 0; j < gridSize; j ++ ){
       newCopy[i][j] = grid[i][j];
     }
   }
-
+ 
   return newCopy;
 }
-
-
+ 
+// calls the vertical operation functions
 function verticalOperate(){
-  if (keyState === "up") {
+  if (direction === "up") {
     grid = moveUp(grid);
     grid = addNumber(grid);
     grid = moveUp(grid);
   }
-
-  if (keyState === "down") {
+ 
+  if (direction === "down") {
     grid = moveDown(grid);
     grid = addNumber(grid);
     grid = moveDown(grid);
   }
 }
-
+ 
+// call the horizontal operation functions
 function horizontalOperate(row) {  
-
-  if (keyState === "right") {
+ 
+  if (direction === "right") {
     row = moveRight(row);
     row = addNumber(row);
     row = moveRight(row);
     return row;
   }
-
-  if (keyState === "left") {
+ 
+  if (direction === "left") {
     row = moveLeft(row);
     row = addNumber(row);
     row = moveLeft(row);
     return row;
   }
 }
-
-
+ 
+ 
 // vertical moves
-
+ 
 // vertical move call function
 function moveVertical() {
   let newCopy = gridCopy(grid);
@@ -179,9 +185,9 @@ function moveVertical() {
   let changed = compareGrids(newCopy, grid);
   if (changed) {
     spawnNumber();
-  } 
+  }
 }
-
+ 
 // move up
 function moveUp(grid) {
   for (let y = 1; y < gridSize; y ++) {
@@ -193,7 +199,7 @@ function moveUp(grid) {
           // swapWithAbove
           grid[thisY -1 ][thisX] = grid[thisY][thisX];
           grid[thisY][thisX] = 0;
-
+ 
           //change thisY
           thisY = thisY - 1;
         }        
@@ -202,10 +208,10 @@ function moveUp(grid) {
   }
   return grid;
 }
-
+ 
 // move down
 function moveDown(grid) {
-  for (let y=3; y >= 0; y--) {   
+  for (let y=3; y >= 0; y--) {  
     for (let x=3; x >= 0; x--) {
       if (grid[y][x] !== 0) {
         let thisY = y;  
@@ -214,7 +220,7 @@ function moveDown(grid) {
           // swap with below
           grid[thisY + 1][thisX] = grid[thisY][thisX];
           grid[thisY][thisX] = 0;
-
+ 
           //change thisY
           thisY = thisY + 1;
         }
@@ -223,9 +229,9 @@ function moveDown(grid) {
   }
   return grid;
 }
-
+ 
 // Horizontal moves
-
+ 
 // Horizontall movement call function
 function moveHorizontal() {
   let newCopy = gridCopy(grid);
@@ -235,9 +241,9 @@ function moveHorizontal() {
   let changed = compareGrids(newCopy, grid);
   if (changed) {
     spawnNumber();
-  } 
+  }
 }
-
+ 
 // move right
 function moveRight(row) {
   let newArray = row.filter(val => val);
@@ -246,7 +252,7 @@ function moveRight(row) {
   newArray = zeros.concat(newArray);
   return newArray;
 }
-
+ 
 // move left
 function moveLeft(row) {
   let newArray = row.filter(val => val);
@@ -255,9 +261,9 @@ function moveLeft(row) {
   newArray = newArray.concat(zeros);
   return newArray;
 }
-
-
-
+ 
+ 
+ 
 // spawn numbers in empty cells
 function spawnNumber() {
   let options = [];
@@ -270,12 +276,12 @@ function spawnNumber() {
     }
   }
   // 50:50 chance of it displaying a 2 or a 4
-  if (options.length > 0) { 
+  if (options.length > 0) {
     // function varibales
     let isZero = false;
     let numberLocation;
     let r = random(0,1);
-
+ 
     // check if a number exist in the cell
     while (!isZero) {
       numberLocation = random (options);
@@ -283,21 +289,19 @@ function spawnNumber() {
         isZero = true;
       }
     }
-
+ 
     // place 2 or a 4
     // place 2
     if (r > 0.25) {
       grid[numberLocation.x][numberLocation.y] = 2;
-      console.log("thing = 2");
     }
     // place 4
     else {
       grid[numberLocation.x][numberLocation.y] = 4;
-      console.log("thing = 4");
     }
   }
 }
-
+ 
 // move numbers to farthest most point of the specified direction until if reaches an obstruction (the end of the grid or a number it is not equivalent to)
 function slide(row) {
   let newArray = row.filter(val => val);
@@ -306,14 +310,14 @@ function slide(row) {
   newArray = zeros.concat(newArray);
   return newArray;
 }
-
+ 
 // think of nested for loop and move cells as needed
-
+ 
 // add numbers that are equivalent when they collide
 function addNumber(row) {
-
+ 
   // horizontal movement addition
-  if (keyState === "right" || keyState === "left"){
+  if (direction === "right"){
     for(let i = 3; i >= 0; i--) {
       let a = row[i];
       let b = row[i -1];
@@ -324,9 +328,21 @@ function addNumber(row) {
     }
     return row;
   }
-
+ 
+  if (direction === "left"){
+    for(let i = 0; i < gridSize; i++) {
+      let a = row[i];
+      let b = row[i + 1];
+      if (a === b) {
+        row[i] = a + b;
+        row[i + 1] = 0;
+      }
+    }
+    return row;
+  }
+ 
   // vertical movement addition
-  if (keyState === "up") {
+  if (direction === "up") {
     for (let y = 1; y < gridSize; y ++) {
       for (let x = 0; x < gridSize; x ++ ){
         let a = grid[y][x];
@@ -339,9 +355,9 @@ function addNumber(row) {
     }
     return grid;
   }
-
-  if (keyState === "down") {
-    for (let y=3; y >= 1; y--) {   
+ 
+  if (direction === "down") {
+    for (let y=3; y >= 1; y--) {  
       for (let x=3; x >= 0; x--) {
         let a = grid[y][x];
         let b = grid[y - 1][x];
@@ -353,9 +369,9 @@ function addNumber(row) {
     }
     return grid;
   }
-  
+ 
 }
-
+ 
 // create & and display grid
 function create2DArray() {
   for (let i = 0; i < gridSize; i ++) {
@@ -367,11 +383,15 @@ function create2DArray() {
       // display numbers
       if (grid[i][j] !== 0) {
         textAlign(CENTER, CENTER);
-        textSize(letterSize);
+        let string = "" + grid[i][j];
+        let len = string.length - 1;
+        let sizes = [64, 64, 32, 16];
         fill(0);
         noStroke();
+        textSize(sizes[len]);
         text(grid[i][j], j * cellWidth + cellWidth / 2, i * cellHeight + cellHeight / 2);
       }
     }
   }
 }
+
