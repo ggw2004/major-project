6bbd92f37058dad4fc41125089ffdb6818aeda7e
ggw2004 <91301141+ggw2004@users.noreply.github.com>
2022-01-14 11:22:03 -0600

    major game state alterations

--- sketch.js
+++ sketch.js
@@ -4,8 +4,8 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
-
+ 
+ 
 // game variables
 let grid = [];
 let gridSize = 4;
@@ -13,19 +13,18 @@ let rows = 4;
 let cols = 4;
 let cellWidth;
 let cellHeight;
+let cornerRound = 30;
  
-// state variables 
+// state variables
 let gameState;
 let direction;
-
+ 
 // text variables
 let mainTextColor = "black";
-let inBoxTextColor = "white";
-let outBoxTextColor = "black";
 let primaryLetterSize;
 let secondaryLetterSize;
 let instructionLetterSize;
-let startingTextBoxBuffer = 15;
+// let startingTextBoxBuffer = 15;
 let textBoxBuffer = 50;
 let generalTextBoxBuffer;
  
@@ -49,70 +48,110 @@ function draw() {
   background(255);
   if (gameState === "starting window") {
     displayText();
-    // gameState = "game setup";
   }
-
+ 
   if (gameState === "instructions"){
     displayText();
   }
-
+ 
   if (gameState === "game setup") {
     createGrid();
  
     // spawn first two numbers
     spawnNumber();
     spawnNumber();
-
+ 
     gameState = "game loop";
   }
-
+ 
   if (gameState === "game loop") {
     cellWidth = width / 4;
     cellHeight = height / 4;
    
     create2DArray();
   }
-
+ 
   if (gameState === "game over") {
-    // code
+    create2DArray();
+    displayText();
+  }
+ 
+  if (gameState === "game complete") {
+    create2DArray();
+    displayText();
   }
 }
-
-
+ 
+ 
 // text function
 function displayText(){
   let messageText;
   let controlText;
-
+  let controlTextPlacement;
+ 
+  // starting window text
   if (gameState === "starting window") {
     messageText = "Welcome to 2048";
-    controlText = "Press the space bar to continue";
-    generalTextBoxBuffer = startingTextBoxBuffer;
+    controlText = "Press the spacebar to continue";
+    controlTextPlacement =  height * 3 / 5 + generalTextBoxBuffer;
+    generalTextBoxBuffer = 15;
     primaryLetterSize = 48;
     secondaryLetterSize = 24;
-    
+   
   }
-
+ 
+ 
+  // game instuction text
   if (gameState === "instructions") {
     messageText = "2048 is a matching game where your job is to combine matching tiles to create larger numbered tiles with the goal of reaching a tile with the value of 2048. When there are no more moves left, and the board is full, the game is over.";
-    controlText = "Game Controls: 'W' and the UP arrow move all tiles up on the grid, 'S' and the DOWN arrow moves all tiles down on the grid, 'A' and the LEFT arrow moves all tiles left of the grid, 'D' and the RIGHT arrow move all tiles to the right of the grid. To start the game press S";
-    primaryLetterSize = 24;
+    controlText = "Game Controls:";
+    let keyInstructionOne = "W' and UP Arrow move all tiles up on the grid";
+    let keyInstructionTwo = "'S' and the DOWN Arrow moves all tiles down on the grid";
+    let keyInstructionThree = "'A' and the LEFT Arrow moves all tiles left of the grid";
+    let keyInstructionFour = "'D' and the RIGHT arrow move all tiles to the right of the grid";
+    let keyInstructionFive = "'B' starts the game";
+    let keyInstructionSix = "'R' restarts the game";
+    primaryLetterSize = 20;
     secondaryLetterSize = 16;
-
+   
     fill(mainTextColor);
     textSize(primaryLetterSize);
     textAlign(CENTER);
     textWrap(WORD);
     rectMode(CENTER);
-    text(messageText, width / 2, height / 2, width - textBoxBuffer, height / 3 * 2);
-    fill(mainTextColor);
+    text(messageText, width / 2, height / 2, width - textBoxBuffer, height / 5 * 4);
     textSize(secondaryLetterSize);
-    text(controlText, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 *3);
+    text(controlText, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 9);
+    text(keyInstructionOne, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 8);
+    text(keyInstructionTwo, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 7);
+    text(keyInstructionThree, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 6);
+    text(keyInstructionFour, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 5);
+    text(keyInstructionFive, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 4);
+    text(keyInstructionSix, width / 2, height * 3 / 5 + generalTextBoxBuffer, width - textBoxBuffer, height / 16 * 3);
   }
-  
-  
-
-  
+ 
+ 
+  // message for if player lost the game
+  if (gameState === "game over") {
+    messageText = "You Lose";
+    controlText = "Press 'R' to Replay";
+    generalTextBoxBuffer = 15;
+    primaryLetterSize = 80;
+    secondaryLetterSize = 60;
+    controlTextPlacement = height / 10 * 7 + generalTextBoxBuffer;
+  }
+ 
+  // message for if plater won the game
+  if (gameState === "game complete") {
+    messageText = "You Won";
+    controlText = "Press 'R' to Replay";
+    generalTextBoxBuffer = 15;
+    primaryLetterSize = 80;
+    secondaryLetterSize = 60;
+    controlTextPlacement = height / 10 * 7 + generalTextBoxBuffer;
+  }
+ 
+ 
   if (gameState !== "instructions") {
     fill(mainTextColor);
     textSize(primaryLetterSize);
@@ -120,12 +159,12 @@ function displayText(){
     text(messageText, width / 2, height / 2);
     fill(mainTextColor);
     textSize(secondaryLetterSize);
-    text(controlText, width / 2, height * 3 / 5 + generalTextBoxBuffer);
+    text(controlText, width / 2, controlTextPlacement);
   }
-
-
+ 
+ 
 }
-
+ 
 // create grid
 function createGrid() {
   for (let i = 0; i < gridSize; i++) {
@@ -138,13 +177,22 @@ function createGrid() {
  
 // Movement Keys
 function keyPressed() {
-
+ 
   if (gameState === "starting window") {
     if (keyCode === 32) { // space bar = 32
       gameState = "instructions";
     }
   }
-
+ 
+  // scenarios when the player may want to restart game
+  if (gameState === "game loop" || gameState === "instructions" || gameState === "game over" || gameState === "game complete") {
+    // keys to start and restart game
+    if (keyCode === 66 || keyCode === 82) { // b = 66 and r = 82
+      gameState = "game setup";
+    }
+  }
+ 
+ 
   // key functions for game loop
   if (gameState === "game loop") {
     // Key to move up
@@ -170,13 +218,20 @@ function keyPressed() {
       direction = "left";
       moveHorizontal();
     }
-   
-    if (isGameOver()) {
+ 
+    // calls function to check if game is over
+    let gameOver = isGameOver();
+    if (gameOver) {
       gameState = "game over";
+    }
+ 
+    // calls function to check if player won the game
+    let gameComplete = isGameWon();
+    if (gameComplete) {
+      gameState = "game complete";
     }
   }
 }
- 
  
 // check if there are any moves left (if the game is over)
 function isGameOver() {
@@ -194,6 +249,18 @@ function isGameOver() {
     }
   }
   return true;
+}
+ 
+// check if player won the game
+function isGameWon() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] === 2048) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
  
  
@@ -385,21 +452,11 @@ function spawnNumber() {
     }
     // place 4
     else {
-      grid[numberLocation.x][numberLocation.y] = 4;
+      grid[numberLocation.x][numberLocation.y] = 4 ;
     }
   }
 }
  
-// move numbers to farthest most point of the specified direction until if reaches an obstruction (the end of the grid or a number it is not equivalent to)
-function slide(row) {
-  let newArray = row.filter(val => val);
-  let missing = gridSize - newArray.length;
-  let zeros = Array(missing).fill(0);
-  newArray = zeros.concat(newArray);
-  return newArray;
-}
- 
-// think of nested for loop and move cells as needed
  
 // add numbers that are equivalent when they collide
 function addNumber(row) {
@@ -460,23 +517,90 @@ function addNumber(row) {
  
 }
  
+ 
+// set tile colors and sizes
+let tiles = {
+  "2": {
+    size: 64,
+    color: "#0B6A88"
+  },
+ 
+  "4": {
+    size: 64,
+    color: "#2DC5F4"
+  },
+ 
+  "8": {
+    size: 64,
+    color: "#70327E"
+  },
+ 
+  "16": {
+    size: 64,
+    color: "#9253A1"
+  },
+ 
+  "32": {
+    size: 64,
+    color: "#A42963"
+  },
+ 
+  "64": {
+    size: 64,
+    color: "#EC015A"
+  },
+ 
+  "128": {
+    size: 36,
+    color: "#F063A4"
+  },
+ 
+  "256": {
+    size: 36,
+    color: "#F16164"
+  },
+ 
+  "512": {
+    size: 36,
+    color: "#F89E4F"
+  },
+ 
+  "1024": {
+    size: 36,
+    color: "#FCEE21"
+  },
+ 
+  "2048": {
+    size: 36,
+    color: "#D20000"
+  }
+};
+ 
 // create & and display grid
 function create2DArray() {
   for (let i = 0; i < gridSize; i ++) {
     for (let j = 0; j < gridSize; j ++ ){
       noFill();
       strokeWeight(2);
+      let val = grid[i][j];
+      let s = val.toString();
       stroke(0);
-      rect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+      if (val !== 0) {
+        // set tile color
+        fill(tiles[s].color);
+      }
+      else {
+        noFill();
+      }
+      rectMode(CORNER);
+      rect(j * cellWidth, i * cellHeight, cellWidth, cellHeight, cornerRound);
       // display numbers
       if (grid[i][j] !== 0) {
         textAlign(CENTER, CENTER);
-        let string = "" + grid[i][j];
-        let len = string.length - 1;
-        let sizes = [64, 64, 32, 16];
-        fill(0);
         noStroke();
-        textSize(sizes[len]);
+        fill(0);
+        // set tile text size
+        textSize(tiles[s].size);
         text(grid[i][j], j * cellWidth + cellWidth / 2, i * cellHeight + cellHeight / 2);
       }
     }
